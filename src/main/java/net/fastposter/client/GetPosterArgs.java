7c570b67eebb9dcac462b081971badc2b33cc0c9
thomas <thomastangweixin@163.com>
2022-12-10 20:06:27 +0800

    v1.1.0 fastposter云服务生成海报接口

--- src/main/java/net/fastposter/client/GetPosterArgs.java
+++ src/main/java/net/fastposter/client/GetPosterArgs.java
@@ -3,18 +3,31 @@ package net.fastposter.client;
 import lombok.Builder;
 import lombok.Data;
 
+/**
+ * 获取海报参数
+ */
 @Data
 @Builder
 public class GetPosterArgs {
 
+    /**
+     * 海报ID
+     */
     Integer id;
 
+    /**
+     * appKey
+     */
     String appKey;
 
+    /**
+     * 载荷
+     */
     String payload;
 
+    /**
+     * 验签
+     */
     String sign;
-
-    Integer timestamp;
 
 }
