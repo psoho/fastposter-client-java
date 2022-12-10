96cd9f29f946208af8e9420ab9f702700145ad78
thomas <thomastangweixin@163.com>
2022-12-11 05:41:33 +0800

    b64演示

--- fastposter-cloud-client-starter-example/src/test/java/cn/fastposter/demo/DemoTests.java
+++ fastposter-cloud-client-starter-example/src/test/java/cn/fastposter/demo/DemoTests.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.FileCopyUtils;
 
 import java.io.File;
 import java.util.HashMap;
