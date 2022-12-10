4de045bea6c6ea43294178df605fa55fae835536
thomas <thomastangweixin@163.com>
2022-12-11 04:03:52 +0800

    调整工程结构

---
+++ fastposter-cloud-client-starter/src/main/java/cn/fastposter/client/autoconfigure/FastposterCloudClientProperties.java
@@ -0,0 +1,46 @@
+package cn.fastposter.client.autoconfigure;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * spring boot starter properties
+ *
+ * @author 阿沐 babamu@126.com
+ */
+@Data
+@ConfigurationProperties("fastposter-cloud")
+public class FastposterCloudClientProperties {
+
+    /**
+     * 接入点
+     */
+    String endpoint;
+
+    /**
+     * appKey
+     */
+    String appKey;
+
+    /**
+     * appSecret
+     */
+    String appSecret;
+
+    /**
+     * 是否打开调试
+     */
+    boolean debug;
+
+    /**
+     * 是否打开追踪
+     */
+    boolean trace;
+
+    /**
+     * 是否预热
+     */
+    boolean warmup;
+
+
+}
