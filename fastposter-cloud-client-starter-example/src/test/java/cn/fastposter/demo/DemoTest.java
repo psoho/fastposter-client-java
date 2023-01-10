b0701b2dc874203da1b13345576fec4a5166fa43
thomas <thomastangweixin@163.com>
2023-01-11 02:15:24 +0800

    获取服务器返回的图片类型

---
+++ fastposter-cloud-client-starter-example/src/test/java/cn/fastposter/demo/DemoTest.java
@@ -0,0 +1,29 @@
+package cn.fastposter.demo;
+
+import cn.fastposter.cloud.client.FastposterCloudClient;
+import cn.fastposter.cloud.client.Poster;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest
+class DemoTest {
+
+    @Autowired
+    FastposterCloudClient client;
+
+    @Test
+    void testPng() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "测试文本");
+        Poster p = client.buildPoster(1000004, params);
+//        p.save(); // 保存海报
+        p.bytes();  // 返回字节数组
+        p.byteStream(); // 返回字节流
+    }
+
+
+}
