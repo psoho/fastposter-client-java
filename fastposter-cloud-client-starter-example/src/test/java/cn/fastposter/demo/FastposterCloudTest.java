5d874512f7fe816822ab40c248a74dc8ca900358
thomas <thomastangweixin@163.com>
2023-01-12 09:59:53 +0800

    测试大图片

--- fastposter-cloud-client-starter-example/src/test/java/cn/fastposter/demo/FastposterCloudTest.java
+++ fastposter-cloud-client-starter-example/src/test/java/cn/fastposter/demo/FastposterCloudTest.java
@@ -1,6 +1,8 @@
 package cn.fastposter.demo;
 
 import cn.fastposter.cloud.client.FastposterCloudClient;
+import cn.fastposter.cloud.client.utils.RandomUtils;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,11 +17,12 @@ class FastposterCloudTest {
     FastposterCloudClient client;
 
     @Test
+    @RepeatedTest(100)
     void testPng() {
 
         // 获取参数
         Map<String, Object> params = new HashMap<>();
-        params.put("name", 100);
+        params.put("name", RandomUtils.randomAlphanumeric(10));
 
         // 生成海报并保存
         client.buildPoster("ced9b1d5337d494c").params(params).build().save();
