b2c2f384003d182b79c6633ede21b251a10e21ad
thomas <thomastangweixin@163.com>
2023-01-14 02:12:39 +0800

    优化代码

--- fastposter-cloud-client-starter-example/src/test/java/net/fastposter/demo/FastposterCloudTest.java
+++ fastposter-cloud-client-starter-example/src/test/java/net/fastposter/demo/FastposterCloudTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class FastposterCloudTest {
